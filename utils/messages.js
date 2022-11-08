07610a328acf7fe0b3fc81ef27b5b3e89f63ccbc
Bryce Guglietti <98065505+bryce-guglietti@users.noreply.github.com>
2022-11-08 17:20:46 -0500

    Update messages.js

--- utils/messages.js
+++ utils/messages.js
@@ -5,7 +5,7 @@ function formatMessage(username, text){
     return {
         username,
         text,
-        time: moment().format('h:mm: a')
+        time: moment.parseZone().format('h:mm: a')
     }
 }
 module.exports = formatMessage;
